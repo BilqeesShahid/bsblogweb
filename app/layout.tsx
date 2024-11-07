7b59bd92f1d781e13fff124b0f3e17080aff7d44
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-11-08 01:26:51 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import {Inter as FontSans} from "next/font/google";
 import { cn } from "@/lib/utils";
@@ -12,17 +11,7 @@ const fontSans= FontSans({
   subsets:["latin"],
   variable: "--font-sans",
 })
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
+ 
 export const metadata: Metadata = {
   title: siteConfig.title,
   description: siteConfig.description,
